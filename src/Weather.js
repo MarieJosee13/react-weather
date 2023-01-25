a20e2fc172c0d3f90dfa6b5c31dc39973ace7dfe
MarieJosee13 <mariejosee1313@gmail.com>
2023-01-25 12:13:23 -0800

    added Bootstrap and fixed up Basic CSS

--- src/Weather.js
+++ src/Weather.js
@@ -3,71 +3,87 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
-      <div>
-        <form>
-          <input
-            className="SearchCity"
-            type="search"
-            placeholder="Enter a city..."
-          ></input>
-          <input type="submit" value="search"></input>
-        </form>
-        <div className="Header">
-          <h1>Vancouver</h1>
+    <div className="Weather container">
+      <form>
+        <div className="row">
+          <div className="col-7">
+            <input
+              className="SearchCity form-control"
+              type="search"
+              placeholder="Enter a city..."
+            />
+          </div>
+          <div className="col-4 p-0">
+            <input className="btn" type="submit" value="search" />
+          </div>
+        </div>
+      </form>
+      <div className="Header">
+        <h1>Vancouver</h1>
+      </div>
+      <div className="row Description">
+        <div className="col-2">
           <img
-            src="https://png.pngtree.com/png-vector/20190627/ourlarge/pngtree-cloudy-icon-design-png-image_1515929.jpg"
+            src="https://cdn-icons-png.flaticon.com/512/3313/3313908.png"
             alt=""
             width="150"
           />
         </div>
-        <div className="Description">
+        <div className="col-2">
           <ul>
             <li>6°C</li>
             <li>Cloudy</li>
           </ul>
+        </div>
+        <div className="col-8">
           <ul>
             <li>Humidity: 97%</li>
             <li>Wind: 10km/h</li>
           </ul>
         </div>
-        <div>
-          <ul className="FiveDay">
-            <li>
-              9°C <br />
-              <img
-                src="https://png.pngtree.com/png-vector/20190627/ourlarge/pngtree-cloudy-icon-design-png-image_1515929.jpg"
-                alt=""
-                width="35"
-              />
-            </li>
-            <li>
-              6°C <br />
-              <img
-                src="https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-partly-cloudy-512.png"
-                alt=""
-                width="30"
-              />
-            </li>
-            <li>
-              5°C
-              <br />
-              <img
-                src="https://png.pngtree.com/png-vector/20190627/ourlarge/pngtree-cloudy-icon-design-png-image_1515929.jpg"
-                alt=""
-                width="35"
-              />
-            </li>
-            <li>
-              7°C
-              <br />
-              <img
-                src="https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-partly-cloudy-512.png"
-                alt=""
-                width="30"
-              />
-            </li>
-          </ul>
+      </div>
+      <div className="row FiveDay">
+        <div className="col-3">
+          <p>
+            9°C <br />
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/3313/3313908.png"
+              alt=""
+              width="35"
+            />
+          </p>
+        </div>
+        <div className="col-3">
+          <p>
+            6°C <br />
+            <img
+              src="https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-partly-cloudy-512.png"
+              alt=""
+              width="30"
+            />
+          </p>
+        </div>
+        <div className="col-3">
+          <p>
+            5°C
+            <br />
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/3313/3313908.png"
+              alt=""
+              width="35"
+            />
+          </p>
+        </div>
+        <div className="col-3 ">
+          <p>
+            7°C
+            <br />
+            <img
+              src="https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-partly-cloudy-512.png"
+              alt=""
+              width="30"
+            />
+          </p>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -26,7 +27,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
