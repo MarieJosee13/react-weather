7d4b5ed351c223704684eeeaf608b726b7a9165a
MarieJosee13 <mariejosee1313@gmail.com>
2023-02-01 13:21:10 -0800

    created a new component that will convert the celsius unit to fahrenheit onClick

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -17,7 +18,7 @@ export default function WeatherInfo(props) {
           />
         </div>
         <div className="col-2">
-          <h3>{Math.round(props.data.temperature)}°C</h3>
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-8">
           <ul>
